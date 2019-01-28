b998ba00578bebed341963a8b99f4cb25a0435ab
Orlin Vasilev <ovasilev@vmware.com>
2019-01-29 00:36:42 +0200

    demo2 recording

---
+++ demo2-docker-psp-taints/demo2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+asciinema play demo2.p1.ascii 
+asciinema play demo2.p2.ascii 
+asciinema play demo2.p3.ascii
+asciinema play demo2.p4.ascii
+asciinema play demo2.p5.ascii
+asciinema play demo2.p6.ascii
+asciinema play demo2.p7.ascii
